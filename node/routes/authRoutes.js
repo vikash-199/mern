b0087a1b7794d05d0157d077a3d4ebc0279b92e4
vikash kumar <vikashkumar24041997@gmail.com>
2026-04-20 00:25:54 +0530

    redis imp

--- node/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { registration } from '../controllers/authController';
-
-const router = express.Router();
-
-router.post('/signup', registration);
-
-export default router;
--- node/utils/generateToken.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '15d' });
-};
-
-export default generateToken;
